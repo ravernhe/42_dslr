87822660bef2e7e2100e74568bb8f6cad414a792
Raphael Vernhes <ravernhe@e3r7p7.42roma.it>
2022-02-02 18:08:00 +0100

    describe to formate

--- srcs/describe.py
+++ srcs/describe.py
@@ -1,5 +1,8 @@
+from cmath import nan
+from math import sqrt
 from typing import Counter
 import pandas as pd
+import numpy as np
 
 # Source : towardsdatascience.com/ Standard Deviation
 # Do :
@@ -53,9 +56,56 @@ def open_set(file_name): # handle error
     return pd.read_csv(file_name)
 
 def main(): #file_name as param : dataset_train.csv
-    df = open_set("../datasets/dataset_train.csv")
+    df = open_set("./datasets/dataset_train.csv")
 
-    desc = describe(df)
+    # desc = describe(df)
+    perc =[.25, .50, .75]
+    include =['float', 'int']
+    desc = df.describe(percentiles = perc, include = include)
+    print(desc)
+    # print(df.axes) //Get column name
+
+    
+    for columns in df.select_dtypes("number").columns.tolist() :
+        # Base describe
+        count = 0
+        total_sum = 0
+        min = 0
+        max = 0
+        for rows in df[columns].sort_values():
+            # print(rows)
+            if pd.notnull(rows) :
+                if (count == 0) :
+                    min = rows
+                    max = rows
+                if (rows > max) :
+                    max = rows
+                total_sum += rows
+                count += 1
+        mean = total_sum / count
+        # Quantile life
+        first_quantile = .25 * (count + 1)
+        second_quantile = .50 * (count + 1)
+        thrid_quantile = .75 * (count + 1)
+        i = 0
+        for rows in df[columns].sort_values():
+            if pd.notnull(rows) :
+                if i == int(first_quantile) :
+                    first_quantile = rows
+                if i == int(second_quantile) :
+                    second_quantile = rows
+                if i == int(thrid_quantile) :
+                    thrid_quantile = rows
+                i += 1
+        # STD PTSD
+        std = 0
+        for rows in df[columns].sort_values():
+            if pd.notnull(rows) :
+                std += (rows - mean) * (rows - mean)
+
+        std = sqrt(std / (count - 1))
+        print("[count]", count, "[Mean]", mean, "[std]", std,"[Min]", min, "[.25]", first_quantile, "[.50]", second_quantile, "[.75]", thrid_quantile, "[Max]", max)
+    
     # desc.count()
     return 0
 
